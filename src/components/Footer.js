a658cc64cb6367e1be469c61ca764d9d1d8fce2b
Younès Fakallah <senyoupl@icloud.com>
2022-02-05 20:28:57 +0100

    Ajouts des états

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,21 +2,30 @@ import { useState } from 'react'
 import '../styles/Footer.css'
 
 function Footer() {
-	const [inputValue, setInputValue] = useState('Ecrivez votre adresse mail ici')
-    const checkBlur = (value) => {
-        if (!value.includes('@')) {
-            alert("Attention, il n'y a pas d'@, ceci n'est pas une adresse valide.")
-        }
-    }
+	const [inputValue, setInputValue] = useState('')
+
+	function handleInput(e) {
+		setInputValue(e.target.value)
+	}
+
+	function handleBlur() {
+		if (!inputValue.includes('@')) {
+			alert("Attention, il n'y a pas d'@, ceci n'est pas une adresse valide 😥")
+		}
+	}
+
 	return (
 		<footer className='lmj-footer'>
 			<div className='lmj-footer-elem'>
 				Pour les passionné·e·s de plantes 🌿🌱🌵
 			</div>
 			<div className='lmj-footer-elem'>Laissez-nous votre mail :</div>
-            <form>
-                <input value={inputValue} onBlur={(a) => checkBlur(a.target.value)} onChange={(a) => setInputValue(a.target.value)}/>
-            </form>
+			<input
+				placeholder='Entrez votre mail'
+				onChange={handleInput}
+				value={inputValue}
+				onBlur={handleBlur}
+			/>
 		</footer>
 	)
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,20 +1,48 @@
 import { plantList } from '../datas/plantList'
-import '../styles/ShoppingList.css'
 import PlantItem from './PlantItem'
+import '../styles/ShoppingList.css'
 
-function ShoppingList() {
-	const categories = Array.from(new Set(plantList.map((plant) => plant.category)))
+function ShoppingList({ cart, updateCart }) {
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
+
+	function addToCart(name, price) {
+		const currentPlantSaved = cart.find((plant) => plant.name === name)
+		if (currentPlantSaved) {
+			const cartFilteredCurrentPlant = cart.filter(
+				(plant) => plant.name !== name
+			)
+			updateCart([
+				...cartFilteredCurrentPlant,
+				{ name, price, amount: currentPlantSaved.amount + 1 }
+			])
+		} else {
+			updateCart([...cart, { name, price, amount: 1 }])
+		}
+	}
 
 	return (
-		<div>
+		<div className='lmj-shopping-list'>
 			<ul>
 				{categories.map((cat) => (
 					<li key={cat}>{cat}</li>
 				))}
 			</ul>
 			<ul className='lmj-plant-list'>
-				{plantList.map(({name, cover, id, light, water}) => (
-					<PlantItem name={name} cover={cover} id={id} light={light} water={water} />
+				{plantList.map(({ id, cover, name, water, light, price }) => (
+					<div key={id}>
+						<PlantItem
+							cover={cover}
+							name={name}
+							water={water}
+							light={light}
+							price={price}
+						/>
+						<button onClick={() => addToCart(name, price)}>Ajouter</button>
+					</div>
 				))}
 			</ul>
 		</div>
